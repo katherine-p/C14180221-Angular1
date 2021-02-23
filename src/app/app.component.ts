bf480a172fb9a3c2ef85eeacfe93f83cb873433f
katherine-p <c14180221@john.petra.ac.id>
2021-02-23 20:58:15 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  board= [["*","*", "*", "*"],
+  ["*", "*", "*", "*"],
+  ["*", "*", "*", "*"],
+  ["*", "*", "*", "*"]];
+
+  in1 : string;
+  ar : string[];
+
+  show(){
+    this.ar = this.in1.split(",");
+  }
+
 }
