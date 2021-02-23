139732f81ce46e3c0eb852788eccb7b83445dc14
katherine-p <c14180221@john.petra.ac.id>
2021-02-23 21:29:27 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,29 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  board= [["*","*", "*", "*"],
-  ["*", "*", "*", "*"],
-  ["*", "*", "*", "*"],
-  ["*", "*", "*", "*"]];
+  board = [["*00","*01", "*02", "*03"],
+  ["*10", "*11", "*12", "*13"],
+  ["*20", "*21", "*22", "*23"],
+  ["*30", "*31", "*32", "*33"]];
 
   in1 : string;
   ar : string[];
+  a : string;
+  index1 : number;
+  index2 : number;
+  board2 : string[][];
 
   show(){
     this.ar = this.in1.split(",");
+    this.index1 = +this.ar[0];
+    this.index2 = +this.ar[1];
+
+    // this.a = this.board[this.index1][this.index2];
+
+    this.board[this.index1][this.index2] = "1";
+    this.a = this.board[this.index1][this.index2];
+    this.board2 = this.board;
+
   }
 
 }
