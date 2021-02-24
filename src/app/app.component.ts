f97bca267a4f7221c049ca33889eb80d04b94ead
katherine-p <c14180221@john.petra.ac.id>
2021-02-24 23:56:38 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,45 +8,115 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  board = [["*00","*01", "*02", "*03", "*04"],
-  ["*10", "*11", "*12", "*13", "*14"],
-  ["*20", "*21", "*22", "*23", "*24"],
-  ["*30", "*31", "*32", "*33", "*34"]];
+  board = [["*","*", "*", "*", "*"],
+  ["*", "*", "*", "*", "*"],
+  ["*", "*", "*", "*", "*"],
+  ["*", "*", "*", "*", "*"],
+  ["*", "*", "*", "*", "*"]];
   
-  in1 : string;
+  inp : string;
   in2 : string;
-  ar : string[];
-  a : string;
+  arr : string[];
   index1 : number;
-  index2 : number;
-  temp : string[][] = this.board;
-  board2 : string[][];
-  board3 : string[][];
-  we = 3;
+  index2 : number;  
+  player = "0";
+  stat : boolean = false;
+  count1 = 0;
+  count0 = 0;
+  winner : string;
 
-  show(){
-    this.ar = this.in1.split(",");
-    this.index1 = +this.ar[0];
-    this.index2 = +this.ar[1];
+  submit()
+  {
+    this.arr = this.inp.split(",");
+    this.index1 = +this.arr[0] - 1;
+    this.index2 = +this.arr[1] - 1;
 
-    // this.a = this.board[this.index1][this.index2];
-    this.board2 = this.temp;
-
-    this.board2[this.index1][this.index2] = "1";
+     if(this.stat == false){
+       if(this.index1 >= 0 && this.index1 <= 4 && this.index2 >= 0 &&this.index2 <= 4){
+         if(this.board[this.index1][this.index2] == "*"){
+           this.board[this.index1][this.index2] = this.player;
+           
+           if(this.player == "1"){
+             this.player = "0";
+           }
+           else{
+             this.player = "1";
+           }
+           this.cekwin();
+         }
+         else{
+           alert("Baris dan kolom sudah terisi");
+         }
+       }
+       else if(this.index1 < 0){
+         alert("Invalid baris kurang dari 1");
+       }
+       else if(this.index2 < 0){
+         alert("Invalid kolom kurang dari 1")
+       }
+       else if(this.index1 > 5){
+         alert("Invalid baris lebih dari 5");
+       }
+       else if(this.index2 > 5){
+         alert("Invalid kolom lebih dari 5")
+       }
+       else{
+         alert("Input tidak sesuai")
+       }
     
-  }
+     }
+   }
 
-  show2(){
-    this.ar = this.in2.split(",");
-    this.index1 = +this.ar[0];
-    this.index2 = +this.ar[1];
+   cekwin(){ 
+    //  cek vertical    
+     for(var b = 0; b < 5; b++){
+       this.count0 = 0;
+       this.count1 = 0;
+       for(var k = 0; k < 5; k++){
+         if(this.board[k][b] == "0"){
+           this.count0++;
+         }
+         else if(this.board[k][b] == "1"){
+           this.count1++;
+         }
+       }
+       if(this.count0 > 4){
+         this.winner = "The winner is Player 0";
+         this.player = "-";
+         this.stat = true;
+       }
+       else if(this.count1 > 4){
+         this.winner = "The winner is Player 1"
+         this.player = "-";
+         this.stat = true;
+       }
+     }
 
-    // this.a = this.board[this.index1][this.index2];
+     //cek horizontal
+     for(var b = 0; b < 5; b++){
+       this.count0 = 0;
+       this.count1 = 0;
+       for(var k = 0; k < 6; k++){
+         if(this.board[b][k] == "0"){
+           this.count0++;
+         }
+         else if(this.board[b][k] == "1"){
+           this.count1++;
+         }
+       }
 
-    this.board2[this.index1][this.index2] = "0";
+       if(this.count0 > 4){
+         this.winner = "The winner is Player 0";
+         this.stat = true;
+       }
+       else if(this.count1 > 4){
+         this.winner = "The winner is Player 1";
+         this.stat = true;
+       }
 
-    this.temp = this.board2; 
-    this.board3 = this.temp;   
-  }
+     }
+   }
+
+   
 
 }
